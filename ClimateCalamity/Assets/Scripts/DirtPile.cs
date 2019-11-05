496b79cfa125e6ce4a12596d494ae6997afd6ff9
Alfie Thomasson <alfiethomasson0@gmail.com>
2019-11-05 22:51:54 +0000

    Adding Basic Game Finished

---
+++ ClimateCalamity/Assets/Scripts/DirtPile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtPile : MonoBehaviour
+{
+    GameObject scoreobj;
+
+    Score scorescript;
+
+     void Awake()
+    {
+        scoreobj = GameObject.Find("ScoreText");
+        if(scoreobj != null)
+        {
+            scorescript = scoreobj.GetComponent<Score>();
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Before: " + collider.gameObject.tag);
+        if(collider.gameObject.tag == "Seed")
+        {
+            Debug.Log("SEED ENTERED");
+            scorescript.ScoreValue += 5;
+            Destroy(collider.gameObject);
+
+        }
+    }
+}
---
+++ ClimateCalamity/Assets/Scripts/RandomPile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPile : MonoBehaviour
+{
+
+    public GameObject pile;
+
+    float chancetospawn;
+    private float nextSpawn = 0;
+    private float randtime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextSpawn)
+        {
+            chancetospawn = Random.Range(0, 5);
+            randtime = Random.Range(2, 4);
+
+            if(chancetospawn == 0)
+            {
+                Instantiate(pile, new Vector2(20, 0), Quaternion.identity);
+            }
+            nextSpawn = Time.time + randtime;
+        }
+    }
+}
