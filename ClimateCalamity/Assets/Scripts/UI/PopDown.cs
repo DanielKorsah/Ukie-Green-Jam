18fc77b4fbb0830c4be344f010f3859b88310569
Dan's Laptop <dkorsah15@aol.co.uk>
2019-11-05 20:04:50 +0000

    bring pause scree up or down

--- ClimateCalamity/Assets/Scripts/UI/PopDown.cs
+++ ClimateCalamity/Assets/Scripts/UI/PopDown.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class PopDown : MonoBehaviour
 {
-    // Start is called before the first frame update
+    RectTransform rt;
+    Vector3 downPos = new Vector3(0, 0, 0);
+    Vector3 upPos = new Vector3(0, 1080, 0);
+    bool isDown;
+
+
+
     void Start()
     {
-        
+        //get the recttransform of the sliding panel 
+        rt = GetComponent<RectTransform>();
+        SetUp();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+
+        //toggle panel up or down with Esc key
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isDown)
+            {
+                SetDown();
+            }
+            else
+            {
+                SetUp();
+            }
+
+        }
+    }
+
+    public void SetDown()
+    {
+        rt.localPosition = downPos;
+        isDown = true;
+        Cursor.visible = true;
+        Time.timeScale = 0;
+    }
+
+    public void SetUp()
+    {
+        rt.localPosition = upPos;
+        isDown = false;
+        Cursor.visible = false;
+        Time.timeScale = 1;
     }
 }
