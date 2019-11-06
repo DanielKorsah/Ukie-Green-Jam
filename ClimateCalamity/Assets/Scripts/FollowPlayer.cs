d2f2131790673f4a75fc5b3b4106982a4f8774a0
Alfie Thomasson <alfiethomasson0@gmail.com>
2019-11-06 09:49:33 +0000

    Adding Stuff

---
+++ ClimateCalamity/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(new Vector3(player.transform.position.x - 0.8, player.transform.position.y - 0.14f), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(player.transform.position.x - 0.8, player.transform.position.y - 0.14), 0);
+    }
+}
